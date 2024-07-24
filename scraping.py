37fbceb302231bfb7dcda29b1830b74358fe43a3
Al-spce <al21003@bristol.ac.uk>
2024-07-24 16:43:14 +0100

    added code

---
+++ scraping.py
@@ -0,0 +1,6 @@
+# %%
+print('what is webscraping?') 
+print(' it is the process of colleceting data from websites using an automated process known as web scraping') 
+# %%
+>>> from urllib.request import urlopen
+>>> url = "http://olympus.realpython.org/profiles/aphrodite"
